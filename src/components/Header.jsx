39af8e74f388e00e21c026d44a474d2fbf5081d8
Anton Potapov <panton160786@gmail.com>
2023-04-23 22:17:00 +0930

    links to repo have added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 function Header() {
     return <nav className="blue lighten-1">
             <div className="nav-wrapper">
-                <a href="#" className="brand-logo">React online store</a>
+                <a href="/" className="brand-logo">React online store</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="#">Repo</a></li>
+                    <li><a href="https://github.com/Panton1986/react_e-commerce" target='_blank' rel="noreferrer">Repo</a></li>
                 </ul>
             </div>
         </nav>
